1026e0222a142441d99865a4de85289a28e7ffe9
NanaMuffin <tietje.hh@gmail.com>
2016-06-11 20:35:03 +0200

    Add files via upload

---
+++ SaveManagerListener.java
@@ -0,0 +1,23 @@
+package lib;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class SaveManagerListener extends WindowAdapter {
+	
+	private StudyManager manager = new StudyManager();
+	
+	public SaveManagerListener(StudyManager manager) {
+		this.manager = manager;
+	}
+	
+	@Override
+	public void windowClosing(WindowEvent e) {
+		StateSaver.saveState(manager);
+		System.out.println("manager gespeichert");
+	}
+	
+
+	
+}
+
