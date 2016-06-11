a01291de900a6d0fc12720734dcdf3c8674994e6
NanaMuffin <tietje.hh@gmail.com>
2016-06-11 20:33:15 +0200

    Add files via upload

---
+++ CourseButton.java
@@ -0,0 +1,16 @@
+package lib;
+
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+
+public class CourseButton extends JButton{
+	
+	public CourseButton(String title) {
+		super(title);
+		setPreferredSize(new Dimension(140, 70));
+		setMaximumSize(new Dimension(120, 70));
+		setFont(this.getFont().deriveFont(16.0f));
+	}
+
+}
