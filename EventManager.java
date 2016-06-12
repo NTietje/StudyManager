89a46bd01b7994487c8837396d649ed88a4072f3
NanaMuffin <tietje.hh@gmail.com>
2016-06-12 11:11:11 +0200

    Update EventManager.java

--- EventManager.java
+++ EventManager.java
@@ -1,11 +1,7 @@
-package lib;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.GregorianCalendar;
-import java.util.HashMap;
-
-import javax.swing.JButton;
 
 /**
  * EventManager checks which events the user would like to be reminded of on a given day.
@@ -23,14 +19,14 @@ public class EventManager {
 	 * returns a list of events of which the user would like to be reminded at this moment 
 	 * @return ArrayList eventList
 	 */
-	public ArrayList<Date> updateEvents() {
+	public ArrayList<Event> updateEvents() {
 		GregorianCalendar today = new GregorianCalendar();
-		ArrayList<Date> eventList = new ArrayList<Date>();
-		Date event;
+		ArrayList<Event> eventList = new ArrayList<Event>();
+		Event event;
 		ArrayList<Semester> semesters = manager.getSemesters();
 		for (int i = 0; i < semesters.size(); i++){
 			Semester semester = semesters.get(i);
-			ArrayList<Date> semesterEvents = semester.getEvents();
+			ArrayList<Event> semesterEvents = semester.getEvents();
 			for (int l = 0; l < semesterEvents.size(); l++){
 				event = semesterEvents.get(l);
 				if (event.remind(today)){
@@ -40,7 +36,7 @@ public class EventManager {
 			ArrayList<Course> courses = semester.getCourses();
 			for (int j = 0; j < courses.size(); j++){
 				Course course = courses.get(j);
-				ArrayList<Date> courseEvents = course.getEvents();
+				ArrayList<Event> courseEvents = course.getEvents();
 				for (int k = 0; k < courseEvents.size(); k++){
 					event = courseEvents.get(k);
 					if (event.remind(today)){
